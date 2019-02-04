e43d166b3b6e86fbf8559c679192c3fb73650bec
Jahnke <naterased@gmail.com>
2019-02-04 13:53:08 -0600

    Axios get for launch id's and details of each launch.

--- src/App.js
+++ src/App.js
@@ -8,24 +8,67 @@ export default class App extends React.Component {
   state = {
     name: undefined,
     time: undefined,
-    location: undefined
+    location: undefined,
+    launchId: []
   };
 
+  // componentDidMount() {
+  //   axios.get("https://launchlibrary.net/1.3/launch/1434").then(res => {
+  //     console.log(res.data.launches[0].name);
+  //     // console.log(res.data.launches[0].name);
+  //     let name = res.data.launches[0].name;
+  //     let time = res.data.launches[0].windowstart;
+  //     let location = res.data.launches[0].location.name;
+  //     this.setState({
+  //       name,
+  //       location,
+  //       time
+  //     });
+  //   });
+  // }
+
+  // componentDidMount() {
+  //   axios.get("https://launchlibrary.net/1.3/launch").then(launches => {
+  //     console.log(launches.data.launches);
+  //   });
+  // }
+
   componentDidMount() {
-    axios.get("https://launchlibrary.net/1.3/launch/1434").then(res => {
-      console.log(res.data.launches[0].name);
-      let name = res.data.launches[0].name;
-      let time = res.data.launches[0].windowstart;
-      let location = res.data.launches[0].location.name;
-      this.setState({
-        name,
-        location,
-        time
+    const rocketLaunches = [];
+    axios.get("https://launchlibrary.net/1.3/launch").then(launches => {
+      launches.data.launches.forEach(rockets => {
+        rocketLaunches.push(rockets.id);
+        // console.log(rocketLaunches);
+      });
+      rocketLaunches.forEach(rocketDetails => {
+        axios
+          .get("https://launchlibrary.net/1.3/launch/" + rocketDetails)
+          .then(launchDetails => {
+            let rocketData = launchDetails.data.launches;
+            this.setState({
+              launchId: [...this.state.launchId, rocketData]
+            });
+          });
       });
     });
   }
 
+  // rocketLaunch = () => {
+  //   axios.get("https://launchlibrary.net/1.3/launch").then(launches => {
+  //     launches.data.launches.forEach(function(rockets) {
+  //       console.log(rockets);
+  //       this.setState({
+  //         launchId: [rockets]
+  //       });
+  //       console.log(this.state.launchId);
+  //     });
+  //   });
+  // };
+
   render() {
+    const json = this.state.launchId[0];
+    console.log(json);
+    // console.log(this.state.launchId[0]);
     return (
       <div>
         <Header />
